d9ebd71425f05efa7b09caf1ced66e08d722a3f8
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-07-23 08:25:33 -0700

    configured app to use next/Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import { RouterOutputs, api } from "~/utils/api";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
@@ -13,7 +14,13 @@ const CreatePostWizard = () => {
   if (!user) return null;
   return (
     <div className="flex w-full gap-3">
-      <img className="w-14 h-14 rounded-full" src={user.profileImageUrl} alt="Profile Image"/>
+      <Image 
+        className="w-14 h-14 rounded-full" 
+        src={user.profileImageUrl} 
+        alt="Profile Image"
+        width={56}
+        height={56}
+      />
       <input className="bg-transparent grow outline-none" placeholder="Type some emojis..." />
     </div>
   );
@@ -25,7 +32,13 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props; 
   return (
     <div className="flex gap-3 p-4 border-b border-slate-400" key={`${post.id}${Math.random()}`}>
-      <img className="w-14 h-14 rounded-full" src={author.profileImageUrl} alt="Post Author Profile Image"/>
+      <Image 
+        className="w-14 h-14 rounded-full" 
+        src={author.profileImageUrl} 
+        alt={`@${author.username}'s Profile Picture`}
+        width={56}
+        height={56}
+      />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
           <span>{`@${author.username}`}</span>
