05887bff19dd756d85d5ce473f926d6b32cec379
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-08-11 03:02:55 -0700

    Render posts on profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,6 +1,22 @@
 import Head from "next/head";
 import type { GetStaticProps, NextPage } from "next";
 import { api } from "~/utils/api";
+import Image from "next/image";
+import {PostView} from '~/components/postview';
+
+const ProfileFeed = (props: {userId: string}) => {
+  const {data, isLoading} = api.posts.getPostsByUserId.useQuery({userId: props.userId});
+  if(isLoading) return <LoadingPage />;
+  //(data as any[]) differs from original code
+
+  if(!data || (data as any[]).length === 0) return <div>User has not posted</div>
+
+  return (
+    <div className="flex flex-col">
+      {(data as any[]).map((fullPost) => (<PostView {...fullPost} key={fullPost.post.id}/>))}
+    </div>
+  );
+};
 
 const ProfilePage: NextPage<{username: string}> = ({username}) => {
   const { data } = api.profile.getUserByUserName.useQuery({
@@ -14,11 +30,22 @@ const ProfilePage: NextPage<{username: string}> = ({username}) => {
       <Head>
         <title>{data.username}</title>
       </Head>
-      <main className="flex justify-center h-screen">
-        <div>
-          {data.username}
+      
+      <PageLayout>
+        <div className="relative h-36 border-slate-400 bg-slate-600">
+          <Image 
+            src={data.profileImageUrl} 
+            alt={`${data.username ?? "" }'s provile image`} 
+            width={128}
+            height={128}
+            className="-mb-[64px] rounded-full border-2 border-black absolute bottom-0 left-0 ml-4"
+          />
         </div>
-      </main>
+        <div className="h-[64px]"/>{/* This is a spacer div creating space btwn profile pic and usernmae */}
+        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400" />
+        <ProfileFeed userId={data.id} />
+      </PageLayout>
     </>
   );
 }
@@ -26,6 +53,8 @@ import { createServerSideHelpers } from '@trpc/react-query/server';
 import { appRouter } from '~/server/api/root';
 import { prisma } from '~/server/db';
 import superjson from "superjson";
+import { PageLayout } from "~/components/layout";
+import { LoadingPage } from "~/components/loading";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createServerSideHelpers({
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -7,7 +7,7 @@ import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/ap
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
-
+import type { Post } from "@prisma/client";
 // Create a new ratelimiter, that allows 3 requests per minute
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
@@ -16,37 +16,52 @@ const ratelimit = new Ratelimit({
   prefix: "@upstash/ratelimit",
 });
 
+const addUserDataToPosts = async (posts: Post[]) => {
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })
+  ).map(filterUserForClient);
+  
+  return posts.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
+    if(!author || !author.username){
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author for post not found"
+      });
+    }
+    return {
+      post, 
+      author: {
+        ...author,
+        username: author.username
+      }
+    }
+  });
+}
+
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query( async ({ ctx }) => {
     const posts =  await ctx.prisma.post.findMany({
       take: 100, // limits number of posts returned to 100 
       orderBy: [{createdAt: 'desc'}]
     });
-
-    const users = (
-      await clerkClient.users.getUserList({
-        userId: posts.map((post) => post.authorId),
-        limit: 100,
-      })
-    ).map(filterUserForClient);
-    
-    return posts.map((post) => {
-      const author = users.find((user) => user.id === post.authorId);
-      if(!author || !author.username){
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Author for post not found"
-        });
-      }
-      return {
-        post, 
-        author: {
-          ...author,
-          username: author.username
-        }
-      }
-    });
+    return addUserDataToPosts(posts);
   }),
+
+  getPostsByUserId: publicProcedure.input(z.object({
+    userId: z.string(),
+  })).query(({ctx, input}) => 
+    ctx.prisma.post.findMany({
+      where: {
+        authorId: input.userId,
+      },
+      take: 100,
+      orderBy: [{ createdAt: "desc" }],
+    }).then(addUserDataToPosts)
+  ),
 
   create: privateProcedure.input(z.object({
     content: z.string().emoji('Only emojis allowed').min(1).max(280),
